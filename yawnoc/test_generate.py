54386e7c659baa65d078eb10b57b15e6301c2569
Conway <yawnoc@pm.me>
2021-07-26 17:32:52 +0800

    Add tests for yawnoc/generate.py parse_sequence_regex_groups

--- yawnoc/test_generate.py
+++ yawnoc/test_generate.py
@@ -33,9 +33,27 @@ class TestStrokeSequenceParsing(unittest.TestCase):
     '(55555|4444|333|22|1|)': {'55555', '4444', '333', '22', '1', ''},
   }
   
+  actual_parse_sequence_regex_groups = {
+    '1': ('1', []),
+    '12345': ('12345', []),
+    '(444)': (r'\1', [['444']]),
+    '(1)(2)(3)(4)(5)': (r'\1\2\3\4\5', [['1'], ['2'], ['3'], ['4'], ['5']]),
+    '()123': (r'\1123', [['']]),
+    '(5|5)43': (r'\143', [['5', '5']]),
+    '(|1)123': (r'\1123', [['', '1']]),
+    '(1|2)(3|45)': (r'\1\2', [['1', '2'], ['3', '45']]),
+    '(|1|11)(|2|22)3': (r'\1\23', [['', '1', '11'], ['', '2', '22']]),
+    '(55555|4444|333|22|1|)':
+      (r'\1', [['55555', '4444', '333', '22', '1', '']]),
+  }
+  
   def test_sequence_set_from_regex(self):
     for regex, set_ in self.actual_sequence_set_from_regex.items():
       self.assertEqual(generate.sequence_set_from_regex(regex), set_)
+  
+  def test_parse_sequence_regex_groups(self):
+    for regex, tuple_ in self.actual_parse_sequence_regex_groups.items():
+      self.assertEqual(generate.parse_sequence_regex_groups(regex), tuple_)
 
 if __name__ == '__main__':
   unittest.main()
