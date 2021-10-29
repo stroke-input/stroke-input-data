stroke-input/stroke-input-data

commit c5bc189a209af0af8447b82bdacf9849f740acf8

Increase decimal places in generate.py print stats

generate.py
@@ -317,21 +317,21 @@ if __name__ == '__main__':
   
   sequences_per_character = sorted_sequence_count / character_count
   print(
-    f'On average, there are {sequences_per_character:.3f} '
+    f'On average, there are {sequences_per_character:.4f} '
     f'stroke sequences per character.'
   )
   
   traditional_fraction = traditional_character_count / character_count
   print(
-    f'{traditional_fraction:5.1%} of characters are traditional-only.'
+    f'{traditional_fraction:5.2%} of characters are traditional-only.'
   )
   
   simplified_fraction = simplified_character_count / character_count
   print(
-    f'{simplified_fraction:5.1%} of characters are simplified-only.'
+    f'{simplified_fraction:5.2%} of characters are simplified-only.'
   )
   
   dual_fraction = 1 - traditional_fraction - simplified_fraction
   print(
-    f'{dual_fraction:5.1%} of characters are dual.'
+    f'{dual_fraction:5.2%} of characters are dual.'
   )

commit d6e737c71c20311f87a021f4f05adac38a0b76c8

Fix generate.py broken stats alignment caused by c5bc189
When increasing the number of decimal places,
you also need to increase the preallocated width

generate.py
@@ -323,15 +323,15 @@ if __name__ == '__main__':
   
   traditional_fraction = traditional_character_count / character_count
   print(
-    f'{traditional_fraction:5.2%} of characters are traditional-only.'
+    f'{traditional_fraction:6.2%} of characters are traditional-only.'
   )
   
   simplified_fraction = simplified_character_count / character_count
   print(
-    f'{simplified_fraction:5.2%} of characters are simplified-only.'
+    f'{simplified_fraction:6.2%} of characters are simplified-only.'
   )
   
   dual_fraction = 1 - traditional_fraction - simplified_fraction
   print(
-    f'{dual_fraction:5.2%} of characters are dual.'
+    f'{dual_fraction:6.2%} of characters are dual.'
   )