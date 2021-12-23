66e1ab276cb2b82cc8c8eafaaae4a1d9337d57f8
Conway <yawnoc@pm.me>
2021-12-24 02:08:44 +0800

    Add unit tests for sort.py character sorting key

--- test_sort.py
+++ test_sort.py
@@ -19,6 +19,26 @@ class TestSort(unittest.TestCase):
   def test_backupise_name(self):
     self.assertEqual(sort.backupise_name('foo', 1234), '.foo-1234.bak')
     self.assertEqual(sort.backupise_name('bar', 0), '.bar-0.bak')
+  
+  simple_sorting_key_from_character = {
+    
+    '㐀': (1, 0x3400),
+    '䶵': (1, 0x4DB5),
+    '䷿': (1, 0x4DFF),
+    
+    '一': (0, 0x4E00),
+    '鿐': (0, 0x9FD0),
+    '鿿': (0, 0x9FFF),
+    
+    'ꀀ': (1, 0xA000),
+    '𠀋': (1, 0x2000B),
+    '𪚲': (1, 0x2A6B2),
+    
+  }
+  
+  def test_simple_character_sorting_key(self):
+    for character, key in self.simple_sorting_key_from_character.items():
+      self.assertEqual(sort.simple_character_sorting_key(character), key)
 
 
 if __name__ == '__main__':
