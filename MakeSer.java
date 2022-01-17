8fdc0f417b3ad2b78c11e86c7b9dafe3a44cad1e
Conway <yawnoc@pm.me>
2022-01-17 17:15:08 +0800

    Create MakeSer.java (currently Hello World)

---
+++ MakeSer.java
@@ -0,0 +1,30 @@
+/*
+  # MakeSer.java
+  
+  Parse the files
+  
+  - `compiled/phrases-traditional.txt`
+    `compiled/phrases-simplified.txt`
+  
+  - `compiled/ranking-traditional.txt`
+    `compiled/ranking-simplified.txt`
+  
+  - `generated/characters-traditional.txt`
+    `generated/characters-simplified.txt`
+  
+  - `generated/sequence-characters.txt`
+  
+  generating the serial files in `generated/`.
+  (Assumes `make_txt.py` has already been run.)
+  
+  Licensed under "MIT No Attribution" (MIT-0),
+  see <https://spdx.org/licenses/MIT-0>.
+*/
+
+public class MakeSer
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world!"); // TODO: actual implementation
+  }
+}
